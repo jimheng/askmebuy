0abcc011cd98dda37329cb8eb50d2e77b770e6e8
jimheng <jimheng@askmebuy.com>
2015-10-21 17:02:07 +0800

    - Askmebuy Easy Buy Script

---
+++ askmebuy-easy-buy.js
@@ -0,0 +1,1 @@
+;(function(){var init = function(){var url = window.location.href;var encodeurl = encodeURIComponent(url);var passurl = 'http://askmebuy.com/en/new-taobao-order.html?url='+encodeurl;window.location.href = passurl;}init();})(window,document);
